5584113a0258fd213e6c0545fd601f3ba0f3d5db
Sun970324 <ysw5202222@gmail.com>
2022-08-11 17:44:57 +0900

    20220811

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
-class Block {
-  constructor(private data: string) {}
-  static hello() {
-    return "hi";
+import crypto from 'crypto';
+
+interface BlockShape {
+  hesh: string;
+  prevHesh: string;
+  height: number;
+  data: string;
+}
+
+class Block implements BlockShape {
+  public hesh: string;
+  constructor(
+    public prevHesh: string,
+    public hegiht: number,
+    public data: string
+  ) {
+    this.hesh = Block.calculateHesh(prevHesh, hegiht, data);
+  }
+  static calculateHesh(prevHesh: string, hegiht: number, data: string) {
+    const toHesh = `${prevHesh}${hegiht}${data}`;
   }
 }
