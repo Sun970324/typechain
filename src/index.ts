6f8aba2cb24cb9619f6c804e7220d3d7215338a5
Sun970324 <ysw5202222@gmail.com>
2022-08-25 19:50:30 +0900

    comp

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,47 @@
-import crypto from 'crypto';
+import crypto from "crypto";
 
 interface BlockShape {
-  hesh: string;
-  prevHesh: string;
+  hash: string;
+  prevHash: string;
   height: number;
   data: string;
 }
 
 class Block implements BlockShape {
-  public hesh: string;
+  public hash: string;
   constructor(
-    public prevHesh: string,
-    public hegiht: number,
+    public prevHash: string,
+    public height: number,
     public data: string
   ) {
-    this.hesh = Block.calculateHesh(prevHesh, hegiht, data);
+    this.hash = Block.calculateHash(prevHash, height, data);
   }
-  static calculateHesh(prevHesh: string, hegiht: number, data: string) {
-    const toHesh = `${prevHesh}${hegiht}${data}`;
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`;
+    return crypto.createHash("sha256").update(toHash).digest("hex");
   }
 }
+
+class BlockChain {
+  private blocks:Block[]
+  constructor () {
+    this.blocks = []
+  }
+  private getPrevHash() {
+    if(this.blocks.length === 0) return ""
+    return this.blocks[this.blocks.length-1].hash
+  }
+  public addBlock(data:string) {
+    const newBlock = new Block(this.getPrevHash(), this.blocks.length +1, data)
+    this.blocks.push(newBlock)
+  }
+  public getBlocks() {
+    return [...this.blocks]
+  }
+}
+
+const blockChain = new BlockChain()
+blockChain.addBlock("First one")
+blockChain.addBlock("Second one")
+blockChain.addBlock("Third one")
+console.log(blockChain.getBlocks());
